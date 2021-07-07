73addef91b1218d607c9934c75849ffef56f87ce
Nakamura Ryuta <nakamura@NakamuranoMacBook-Pro.local>
2021-07-07 16:31:13 +0700

    add: 'add' component

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { AddComponent } from './add/add.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AddComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
