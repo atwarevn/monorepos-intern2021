c5e5823bd5560cfba37c8dd6ca5b17335dfdc07e
Phan Long Hieu <phanlonghieu@MBP-cua-Phan.fritz.box>
2021-07-07 14:04:21 +0700

    complete draf of front end

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from './Todo.interface';
+import { TodoService } from './Todo.service';
 
 @Component({
   selector: 'monorepos-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'frontend';
+
+
+export class AppComponent implements OnInit{
+  todos:  Todo[] = [];
+
+  constructor(private todoService:TodoService){}
+
+  ngOnInit(){
+    this.todoService.getAllTodos()
+    .subscribe(res => this.todos = res);
+  }
+
+
 }
